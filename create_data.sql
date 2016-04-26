81f6fa0a7e6975d99061da5bc72d79441067c90c
MingxiaoGuo <marshallguo0221@gmail.com>
2016-04-26 15:39:35 -0700

    no message

---
+++ create_data.sql
@@ -0,0 +1,6 @@
+insert into user values (1, 'm', 'guo', 'mg@sjsu.edu', 'm', '1', date(now()), '123', 'a', 'san jose', 'ca', '95110');
+insert into category values(1, 'test', 'test category');
+insert into service values(1, 'test', 'test video', 'image', 'this is a test', 'san jose', 'ca', 1, date(now()), 1);
+insert into service values(2, 'test2', 'test2 video', 'image2', 'this is a test2', 'san jose', 'ca', 1, date(now()), 1);
+insert into service values(3, 'test3', 'test3 video', 'image3', 'this is a test3', 'san jose', 'ca', 1, date(now()), 1);
+insert into service values(4, 'test4', 'test4 video', 'image4', 'this is a test4', 'san jose', 'ca', 1, date(now()), 1);
